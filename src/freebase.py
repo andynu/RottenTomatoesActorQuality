dff2d847ca0eff5d81dc7f995a011b44049c73e6
andy <andynu@gmail.com>
2014-02-10 18:46:56 -0500

    get all the movies.

--- src/freebase.py
+++ src/freebase.py
@@ -1,27 +1,38 @@
 import json
 import urllib
 
-api_key = open(".freebase_api_key").read()
+api_file = '/home/andy/RottenTomatoesActorQuality/.freebase_api_key'
+api_key = open(api_file).read()
 service_url = 'https://www.googleapis.com/freebase/v1/search'
 
 def search(params):
   params['key'] = api_key
   url = service_url + '?' + urllib.urlencode(params)
   response = json.loads(urllib.urlopen(url).read())
-  return response['result']
+  result = response['result']
+  return result
+
+def actor(name):
+  return search({
+    'filter':'(all name:"'+name+'" type:/film/actor)'
+  })[0] # hope it is the first hit.
+
+def all_actors():
+  return search({
+    'filter':'(all type:/film/actor)',
+    'limit':'200'
+  })
+
+def movies(actor_name):
+  return search({
+    'filter':'(all type:/film/film contributor:"'+actor_name+'")',
+    'limit':'200'
+  })
 
 if __name__ == '__main__':
+  for actor in all_actors():
+    print
+    print actor['name']
+    for movie in movies(actor['name']):
+      print movie['name']
 
-  name = 'Steve Martin'
-  print name
-
-  actor_query = {'filter':'(all name:"'+name+'" type:/film/actor)'}
-  actor = search(actor_query)[0] # hope it is the first hit.
-
-  movies_query = {
-    'filter':'(all type:/film/film contributor:"'+actor['name']+'")',
-    'limit':'200'
-    }
-  
-  for movie in search(movies_query):
-    print '%(name)s (%(score)f)' % movie
